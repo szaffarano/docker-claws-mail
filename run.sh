66f7a1a6fad813aa7ba573b561f425372288da42
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2017-08-28 15:20:24 -0300

    Add volume bindings for time zone related files

--- run.sh
+++ run.sh
@@ -5,5 +5,7 @@ IMAGE="szaffarano/claws-mail:3.15.0"
 docker run --rm \
   -e DISPLAY=unix$DISPLAY \
   -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+  -v /etc/timezone:/etc/timezone:ro \
+  -v /etc/localtime:/etc/localtime:ro \
   -v "${CLAWS_HOME:-$HOME/claws}":"/home/user" \
   ${IMAGE} $@
